70bbb38bcb771191eae99ac5262c53ad4450f4ca
akhil592 <akhil.592@gmail.com>
2023-02-19 20:46:02 -0800

    Update vvas_xpp_pipeline.c

--- src/vvas_xpp_pipeline.c
+++ src/vvas_xpp_pipeline.c
@@ -169,7 +169,7 @@ int32_t xlnx_kernel_start(VVASKernel *handle, int start, VVASFrame *input[MAX_NU
     LOG_MESSAGE (LOG_LEVEL_INFO, "Input width %d", input[0]->props.width);
     LOG_MESSAGE (LOG_LEVEL_INFO, "Input height %d", input[0]->props.height);
     LOG_MESSAGE (LOG_LEVEL_INFO, "Input stride %d", input[0]->props.stride);
-   // LOG_MESSAGE (LOG_LEVEL_INFO, "Input stride has been reset to the following ****** %d", 256);
+    LOG_MESSAGE (LOG_LEVEL_INFO, "Input stride has been reset to the following ****** %d", input[0]->props.width);
     LOG_MESSAGE (LOG_LEVEL_INFO, "output width %d", output[0]->props.width);
     LOG_MESSAGE (LOG_LEVEL_INFO, "output height %d", output[0]->props.height);
     LOG_MESSAGE (LOG_LEVEL_INFO, "output stride %d", output[0]->props.width);
@@ -181,7 +181,7 @@ int32_t xlnx_kernel_start(VVASKernel *handle, int start, VVASFrame *input[MAX_NU
         (kernel_priv->params->paddr[0]),
         (input[0]->props.width),
         (input[0]->props.height),
-        (input[0]->props.stride),
+        (input[0]->props.width),
         (output[0]->props.width),
         (output[0]->props.height),
         (output[0]->props.width)
